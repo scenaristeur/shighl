e94242f6e95b605deb86bf05137d6690c232d0bf
David <scenaristeur@gmail.com>
2020-02-01 21:56:37 +0100

    add functions

--- README.md
+++ README.md
@@ -7,13 +7,21 @@ S-olid HIGH L-evel
 - based on ldflex-query
 - use it in your project ? just copy  /dist/window/shighl.bundle.js to tour folder and import with
 ```
-  <script src="window/shighl.bundle.js"> </script>
-  <script>
-  const sh = new Shighl()
-  console.log(sh)
-  sh.test()
-  </script>
-  ```
+<script src="window/shighl.bundle.js"> </script>
+<script>
+const sh = new Shighl()
+console.log(sh)
+sh.test()
+</script>
+```
+- or import
+```
+import  Shighl  from 'shighl'
+...
+const sh = new Shighl()
+console.log(sh)
+sh.test()
+```
 
 see /dist/index.html
 
@@ -39,17 +47,17 @@ npm run start
 <!doctype html>
 <html lang="en">
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-  <title>Shighl</title>
-  <script src="window/shighl.bundle.js"> </script>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<title>Shighl</title>
+<script src="window/shighl.bundle.js"> </script>
 </head>
 <body>
 
-  <h1>Shigh</h1>
-  <ul id="friends">
-  </ul>
+<h1>Shigh</h1>
+<ul id="friends">
+</ul>
 
 </body>
 
@@ -68,29 +76,29 @@ async function run(){
     var t = document.createTextNode(f.webId);
     li.appendChild(t);
     friends_ul.appendChild(li)
-  })
-}
+    })
+  }
 
-run()
-
-
-</script>
-
-</html>
+  run()
 
 
-```
+  </script>
+
+  </html>
+
+
+  ```
 
 
 
 
-# Loading from https://cdn.jsdelivr.net
+  # Loading from https://cdn.jsdelivr.net
 
-```
+  ```
 
-<!doctype html>
-<html lang="en">
-<head>
+  <!doctype html>
+  <html lang="en">
+  <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -102,47 +110,50 @@ run()
   <link href="css/fontawesome/css/all.css" rel="stylesheet">
   <script src="vendor/jquery/jquery.slim.min.js"></script>-->
   <script src="https://cdn.jsdelivr.net/gh/scenaristeur/shighl@master/dist/window/shighl.bundle.js"> </script>
-</head>
-<body>
+  </head>
+  <body>
 
   <h1>Shigh</h1>
   <ul id="friends">
   </ul>
 
-</body>
+  </body>
 
-<script>
+  <script>
 
-const sh = new Shighl()
-console.log(sh)
-sh.test()
+  const sh = new Shighl()
+  console.log(sh)
+  sh.test()
 
-async function run(){
-  let friends  = await sh.getFriends("https://spoggy.solid.community/profile/card#me")
-  console.log(friends)
-  const friends_ul = document.getElementById("friends")
-  friends.forEach(function(f){
-    var li = document.createElement("LI")
-    var t = document.createTextNode(f.webId);
-    li.appendChild(t);
-    friends_ul.appendChild(li)
-  })
-}
+  async function run(){
+    let friends  = await sh.getFriends("https://spoggy.solid.community/profile/card#me")
+    console.log(friends)
+    const friends_ul = document.getElementById("friends")
+    friends.forEach(function(f){
+      var li = document.createElement("LI")
+      var t = document.createTextNode(f.webId);
+      li.appendChild(t);
+      friends_ul.appendChild(li)
+      })
+    }
 
-run()
-
-
-</script>
-
-</html>
-
-```
+    run()
 
 
+    </script>
+
+    </html>
+
+    ```
 
 
 
-# webpack build copied from @jeffz https://jeff-zucker.github.io/solid-file-client
 
-- build & publish to gh-pages
-npm run build && git subtree push --prefix dist origin gh-pages
+
+    # webpack build copied from @jeffz https://jeff-zucker.github.io/solid-file-client
+
+    - build & publish to gh-pages
+    npm run build && git subtree push --prefix dist origin gh-pages
+
+
+    https://blog.jakoblind.no/webpack-code-splitting-libraries-what-to-do-with-the-files/
--- src/Shighl.js
+++ src/Shighl.js
@@ -2,15 +2,11 @@ import * as auth from 'solid-auth-client';
 import data from "@solid/query-ldflex";
 
 class Shighl {
-  /**
-  * @param {SolidAuthClient} auth - An auth client, for instance solid-auth-client or solid-auth-cli
-  * @param {SolidFileClientOptions} [options]
-  */
   constructor () {
-    console.log("ok")
-    /*  super(auth.fetch.bind(auth), options)
-    this._auth = auth*/
-  }
+    console.log("Shighl loaded")
+      this.webId = null
+      this.friends = []
+    }
 
   async test(){
     var name = await data['https://spoggy.solid.community/profile/card#me'].vcard$fn
@@ -27,8 +23,45 @@ class Shighl {
     }
   }
 
+  trackSession(cb) {
+    var module = this
+    auth.trackSession(async function(session) {
+      if (!session){
+        module.webId = null
+      }else{
+        module.webId = session.webId
+      }
+      cb(module.webId)
+    })
+  }
+
+  login(cb) {
+    this.popupLogin().then((session) => cb(session.webId));
+  }
+
+  logout(cb) {
+    auth.logout().then(() => cb());
+  }
+
+  async popupLogin() {
+    let session = await auth.currentSession();
+    let popupUri = './dist-popup/popup.html';
+    if (!session)
+    {
+      return session = await auth.popupLogin({ popupUri });
+    }else{
+      alert("You are already logged")
+      return session
+    }
+  }
+
+  get getWebId(){
+    return this.webId
+  }
+
+
   async getFriends(webId = this.webId){
-    //  console.log(webId)
+    console.log(webId)
     this.friends = []
     for await (const fwebid of data[webId].friends){
       //  console.log(friend)
@@ -36,9 +69,148 @@ class Shighl {
       friend.webId = `${fwebid}`
       this.friends = [... this.friends, friend]
     }
-    console.log(this.friends)
     return this.friends
   }
+
+
+  async getName(){
+    console.log(this.webId)
+    try {
+      const fullname = await data.user.vcard$fn;
+      console.log(`\nNAME: ${fullname}`);
+      return `${fullname}`
+    }catch(e){
+      return e
+    }
+  }
+
+
+  async getPublicTypeIndex(webId){
+    console.log(webId)
+    var pti = {}
+    pti.url = await data[webId].publicTypeIndex
+    try{
+      if (`${pti.url}` != "undefined"){
+        pti.instances = []
+        for await (const subject of data[pti.url].subjects){
+          //  console.log(`${subject}`);
+          if (`${pti.url}` != `${subject}`) {
+            const s = {subject: `${subject}`}
+            for await (const property of subject.properties)
+            {
+              if (`${property}` == "http://www.w3.org/ns/solid/terms#instance")    {
+                //  console.log( "--",`${property}`);
+                const instance = await data[subject][`${property}`]
+                const classe = await data[subject].solid$forClass
+                //  console.log( "--nn",`${instance}`);
+                s.predicate = `${property}`
+                s.object = `${instance}`
+                s.classe = `${classe}`
+                //  s.path = `${instance}`.split("/")
+                s.shortClasse = this.localName(s.classe)
+              }
+            }
+            pti.instances.push(s)
+          }
+        }
+      }
+    }catch(e){
+      console.log(e)
+    }
+    return pti
+  }
+
+  async getPages(folder){
+    var pages = {}
+    var years = []
+    for await (const year of data[folder]['ldp$contains']){
+      //  console.log("YEAR",`${year}`);
+      if ( `${year}`.endsWith('/')){
+        var localyear = this.localName(`${year}`.slice(0, -1))
+        years.push(localyear)
+      }
+    }
+    //  console.log(years)
+    var last_year = Math.max(...years)
+
+    //MONTH
+    var months = []
+    for await (const month of data[folder+last_year+'/']['ldp$contains']){
+      //  console.log("MONTH",`${month}`);
+      if ( `${month}`.endsWith('/')){
+        var localmonth = this.localName(`${month}`.slice(0, -1))
+        months.push(localmonth)
+      }
+    }
+    //  console.log(months)
+    var last_month = ("0" + Math.max(...months)).slice(-2)
+
+    //DAY
+    var days = []
+    for await (const day of data[folder+last_year+'/'+last_month+'/']['ldp$contains']){
+      //  console.log("DAY",`${day}`);
+      if ( `${day}`.endsWith('/')){
+        var localday = this.localName(`${day}`.slice(0, -1))
+        days.push(localday)
+      }
+    }
+    //console.log(days)
+    var last_day = ("0" + Math.max(...days)).slice(-2)
+    //  console.log("Last day",last_day)
+
+    pages.years = years.sort()
+    pages.months = months.sort()
+    pages.days = days.sort()
+    pages.year = last_year
+    pages.month = last_month
+    pages.day = last_day
+    pages.folder = folder
+    return pages
+  }
+
+  async getMessages(pages){
+    var messages = []
+    console.log(pages)
+    var path = pages.folder+[pages.year,pages.month,pages.day,""].join('/')
+    //  console.log(path)
+    //console.log("Clear")
+    await data.clearCache()
+    let chatfile = await data[path]['ldp$contains'];
+    //  console.log("ChatFile",`${chatfile}`);
+
+    for await (const subject of data[chatfile].subjects){
+      //  console.log("subject", `${subject}` );
+      if (( `${subject}` != pages.folder) && ( `${subject}` != chatfile) && ( `${subject}` != pages.instance)){ // ne semble pas fonctionner ??
+        messages = [... messages, `${subject}`]
+        //console.log(docs)
+      }
+    }
+    //  console.log(docs)
+    messages.sort().reverse()
+    console.log(messages)
+    return messages
+  }
+
+  async messageDetails(msgurl){
+    console.log(msgurl)
+    var details = {}
+    details.date = await data[msgurl]['http://purl.org/dc/terms/created']
+    return details
+  }
+
+  localName(str){
+    var ln = str.substring(str.lastIndexOf('#')+1);
+    //console.log(ln)
+    ln == str ? ln = str.substring(str.lastIndexOf('/')+1) : "";
+    return ln
+  }
+
+  testCallBack(cb){
+    var test = "this is a test for your callback"
+    cb(test)
+  }
+
+
 
 }
 
