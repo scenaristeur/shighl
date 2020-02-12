132f4048d85bcbe6fe867883888fa04d98f68612
David <scenaristeur@gmail.com>
2020-02-12 01:04:28 +0100

    use mocha in html & create user

---
+++ session-tests.js
@@ -0,0 +1,22 @@
+//const chai = require('chai')
+
+
+
+//const Shighl = require('../src/Shighl')
+
+
+//var assert = require('assert');
+describe('Loading Shighl test', function() {
+  describe('"Spoggy" is vcard$fullname of https://spoggy.solid.community/profile/card#me', function() {
+    it('sh.test() should return "Spoggy"', async () => {
+      let test = await sh.test()
+      expect(test).to.equal("Spoggy")
+    })
+
+    it('sh.test() should return "Spoggy"', async () => {
+      let test = await sh.test()
+      expect(test).to.equal("Spoggy")
+    })
+
+  });
+});
